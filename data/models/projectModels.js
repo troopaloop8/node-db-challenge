b6c6c99783b83c3d18c6712fac48bfbcdc1fc0ad
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-05-30 12:32:07 +0900

    stretcchhhh endpoint done

--- data/models/projectModels.js
+++ data/models/projectModels.js
@@ -7,6 +7,9 @@ module.exports = {
   addProject,
   addResource,
   addTask,
+  getTasksByProjectId,
+  getResourcesByProjectId,
+  getProjectInfoById
 };
 
 //*****CRUD HELPERS*****//
@@ -46,6 +49,29 @@ function getResourcesByProjectId(id) {
     .join("project_resources as pr", "p.id", "pr.project_id")
     .join("resources as r", "pr.resource_id", "r.id")
     .where({ "p.id": id });
+}
+
+function getProjectInfoById(id) {
+  //grab object for project by Id, task by project id, resource by project id
+  const prj = db('projects').where({id}).first();
+  const tsk = getTasksByProjectId(id);
+  const rsc = getResourcesByProjectId(id);
+
+  //combine objects into an array
+  const prjInfo = [prj, tsk, rsc];
+  //pass all the objects in the array into a promise
+  return Promise.all(prjInfo).then(info => {
+    //desctructure the array from the promise
+    const [prj, tsk, rsc] = info;
+
+    //add the task and resrouce objects inside the project object
+    prj.tasks = tsk;
+    prj.resources = rsc;
+
+    //return prj with the nested objects
+    return prj;
+
+  })
 }
 
 
--- projectRoutes.js
+++ projectRoutes.js
@@ -98,6 +98,19 @@ router
         })
     });
 
+router
+    .route("/:id")
+    .get((req, res) => {
+        const {id} = req.params;
+        db.getProjectInfoById(id)
+        .then(projectInfo => {
+            res.status(200).json(projectInfo)
+        })
+        .catch(err => {
+            res.status(500).json({message: `Something went wrong... ${err}, ${err.message}`})
+        })
+    })
+
 
 
 //EXPORT
