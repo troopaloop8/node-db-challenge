308ff7a4beca495623e92e5f6256fb3e63bb67dc
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-05-30 10:39:05 +0900

    created the basic helpers

--- data/models/projectModels.js
+++ data/models/projectModels.js
@@ -1,10 +1,51 @@
-const db = require('../database.js');
+const db = require("../database.js");
 
 module.exports = {
-    getProjects,
-    getResources,
-    getTasks,
-    addProject,
-    addResource,
-    addTask
-}
+  getProjects,
+  getResources,
+  getTasks,
+  addProject,
+  addResource,
+  addTask,
+};
+
+//*****CRUD HELPERS*****//
+
+//READ HELPERS
+
+function getProjects() {
+  return db("projects");
+};
+
+function getResources(id) {
+  if (id) {
+    return db("resources").where({ id });
+  } else {
+    return db("resources");
+  };
+};
+
+function getTasks(id) {
+  if (id) {
+    return db("tasks").where({ id });
+  } else {
+    return db("tasks");
+  };
+};
+
+
+
+//CREATE HELPERS
+
+function addProject(project) {
+  return db("projects").insert(project);
+};
+
+function addResource(resource) {
+  return db("resources").insert(resource);
+};
+
+function addTask(task) {
+  return db("tasks").insert(task);
+};
+
